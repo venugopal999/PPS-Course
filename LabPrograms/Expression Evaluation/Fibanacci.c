eb2e0e7457357bb4c583c74bf1e8349bb955b040
venugopal999 <venugopal.gora@gmail.com>
2026-07-18 20:30:23 +0530

    Update Fibanacci.c

--- LabPrograms/Expression Evaluation/Fibanacci.c
+++ LabPrograms/Expression Evaluation/Fibanacci.c
@@ -1,4 +1,4 @@
-// Week - 2: A Fibonacci sequence is defined as follows: the first and second terms in the sequence are 0 and 1. 
+// A Fibonacci sequence is defined as follows: the first and second terms in the sequence are 0 and 1. 
 //Subsequent terms are found by adding the preceding two terms in the sequence. Program to generate the first n terms of the sequence.
 // Objective: Demonstrate usage of loops
 // Author: Venu Goapl
