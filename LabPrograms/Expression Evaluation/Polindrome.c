ff87d26030b21578c7cb985f9496d877231e7905
venugopal999 <venugopal.gora@gmail.com>
2026-07-18 20:21:44 +0530

    Add files via upload

---
+++ LabPrograms/Expression Evaluation/Polindrome.c
@@ -0,0 +1,33 @@
+
+// Week - 2: program to find the sum of individual digits of a positive integer and test given number is palindrome.
+// Objective: Demonstrate usage of loops
+// Author: Venu Goapl
+
+
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, digit, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    original = num;  
+
+    while (num > 0) {
+        digit = num % 10;
+        sum += digit;                 
+        reversed = reversed * 10 + digit;  
+        num /= 10;
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    if (original == reversed)
+        printf("%d is a palindrome.\n", original);
+    else
+        printf("%d is not a palindrome.\n", original);
+
+    return 0;
+}
+
